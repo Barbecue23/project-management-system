54a7677a355e74e1d9bd1af41bb5c22ece3c5ac5
DoLLaRs6252 <punvarattaya6252@gmail.com>
2025-02-24 23:19:23 +0700

    mock data for advisors

---
+++ app/controllers/advisors_controller.rb
@@ -0,0 +1,79 @@
+# app/controllers/advisors_controller.rb
+class AdvisorsController < ApplicationController
+  def index
+    @advisors = [
+      {
+        id: 1,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: "saijaporn.w@gmail.com",
+        current_students: 4,
+        max_students: 6,
+        status: "active"
+      },
+      {
+        id: 2,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: nil,
+        current_students: 6,
+        max_students: 6,
+        status: "active"
+      },
+      {
+        id: 3,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: "saijaporn.w@gmail.com",
+        current_students: 4,
+        max_students: 6,
+        status: "active"
+      },
+      {
+        id: 4,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: "saijaporn.w@gmail.com",
+        current_students: 3,
+        max_students: 6,
+        status: "active"
+      },
+      {
+        id: 5,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: "saijaporn.w@gmail.com",
+        current_students: 6,
+        max_students: 10,
+        status: "active"
+      },
+      {
+        id: 6,
+        name: "อ.ดร.สัจจาภรณ์ ไวยญาณ",
+        position: "Dr.Saijaporn Waijanya",
+        specialty: "Software Engineer",
+        specialty_details: "ดูเพิ่มเติม",
+        email: "waijanya_s@silpakorn.edu",
+        alternate_email: nil,
+        current_students: 3,
+        max_students: 6,
+        status: "active"
+      }
+    ]
+  end
+end
