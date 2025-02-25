6fc22ecb39c22d61a8054af50713a63724dfe16e
Barbecue <sunsanasupphong_j@silpakorn.edu>
2025-02-25 13:51:26 +0700

    Refactor button method indentation in application_helper for improved readability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-    def button(text, type: :primary, disabled: false, icon: nil)
-      style_classes = case type
-      when :primary then "bg-blue-100 text-blue-600 border hover:bg-blue-200"
-      when :secondary then "border border-[#AFAFAF] text-gray-[#888888]"
-      when :disabled then "bg-gray-200 text-gray-400 border border-gray-300"
-      else "border border-gray-400 text-gray-600"
-      end
+  def button(text, type: :primary, disabled: false, icon: nil)
+    style_classes = case type
+                    when :primary then "bg-blue-100 text-blue-600 border hover:bg-blue-200"
+                    when :secondary then "border border-[#AFAFAF] text-gray-[#888888]"
+                    when :disabled then "bg-gray-200 text-gray-400 border border-gray-300"
+                    else "border border-gray-400 text-gray-600"
+                    end
 
     render partial: "shared/button", locals: { text: text, style_classes: style_classes, disabled: disabled, icon: icon }
-    end
+  end
 end
