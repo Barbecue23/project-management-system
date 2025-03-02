31540d0ed2657c492aa2f0915045957dec6f97c3
Soratun <tunzax014@gmail.com>
2025-03-02 11:58:53 +0700

    Add Permission model, migration, fixtures, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_28_110822) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_02_045456) do
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.boolean "default_view", default: false
+    t.boolean "default_create", default: false
+    t.boolean "default_edit", default: false
+    t.boolean "default_delete", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "created_by"
