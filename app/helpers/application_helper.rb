50b1eb99dc35739ed7342176c3d4ab062c7b7d31
DoLLaRs6252 <punvarattaya6252@gmail.com>
2025-03-10 10:56:05 +0700

    [Pun] change to css adviser and button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def button(text, type: :primary, disabled: false, icon: nil)
     style_classes = case type
-    when :primary then "bg-blue-100 text-blue-600 border hover:bg-blue-200"
-    when :secondary then "border border-[#AFAFAF] text-gray-[#888888]"
-    when :disabled then "bg-gray-200 text-gray-400 border border-gray-300"
-    else "border border-gray-400 text-gray-600"
+    when :primary then "btn btn-primary"
+    when :secondary then "btn btn-secondary"
+    when :disabled then "btn btn-disabled"
+    else "btn"
     end
 
     render partial: "shared/button", locals: { text: text, style_classes: style_classes, disabled: disabled, icon: icon }
