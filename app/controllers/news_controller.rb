c76a211d94f72aacc26d50167aed438648a626d1
Soratun <tunzax014@gmail.com>
2025-03-24 23:28:03 +0700

    feat: enhance news creation with public/private categorization and streamline form handling

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -50,29 +50,30 @@ class NewsController < ApplicationController
   end
 
   def new
-    @news = {
-      title: "",
-      content: "",
-      category: "",
-      announcer: "",
-      publish_date: "",
-      banner_image: "",
-      more_images: []
-    }
+    @news = News.new
   end
 
   def create
-    new_news = news_params.to_h
-    new_news[:banner_image] = params[:banner_image].present? ? params[:banner_image] : "loading.svg"
-    new_news[:created_at] = "เพิ่งเพิ่มเมื่อกี้"
+    is_public = params[:category] == "All"
 
-    flash[:notice] = "ข่าวถูกสร้างเรียบร้อยแล้ว (Mock Data)"
-    redirect_to news_index_path
+    @news = News.new(news_params.merge(is_public: is_public ,created_by: "system"))
+
+    if @news.save
+      unless is_public
+        advisor_group_ids = AdvisorGroup.where(group_name: params[:category]).pluck(:id) 
+        advisor_group_ids.each do |advisor_group_id|
+          NewsGroup.create!(news: @news, advisor_group_id: advisor_group_id, created_by: "system")
+        end
+      end
+      redirect_to news_index_path
+    else
+      render :new
+    end
   end
 
   private
 
   def news_params
-    params.permit(:title, :content, :category, :announcer, :publish_date, :banner_image, more_images: [])
+    params.permit(:title, :content ,:publish_date)
   end
 end
