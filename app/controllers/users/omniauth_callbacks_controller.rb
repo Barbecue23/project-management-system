333036df225f667dcc4164426ab96a23abd9bb90
Soratun <tunzax014@gmail.com>
2025-04-12 10:19:04 +0700

    Refactor OAuth2 callback handling: improve code structure and maintainability

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,22 @@
-def oauth2
-  auth = request.env["omniauth.auth"]
-  if auth.blank?
-    redirect_to root_path, alert: "ไม่สามารถรับข้อมูลผู้ใช้ได้"
-    return
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def oauth2
+    auth = request.env["omniauth.auth"]
+    if auth.blank?
+      redirect_to root_path, alert: "ไม่สามารถรับข้อมูลผู้ใช้ได้"
+      return
+    end
+
+    Rails.logger.info "OAuth2: AUTH = #{auth.inspect}"
+
+    @user = User.from_omniauth(auth)
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to root_path, alert: "ไม่สามารถเข้าสู่ระบบได้"
+    end
   end
 
-  Rails.logger.info "OAuth2: AUTH = #{auth.inspect}"
-
-  @user = User.from_omniauth(auth)
-  if @user.persisted?
-    sign_in_and_redirect @user, event: :authentication
-  else
-    redirect_to root_path, alert: "ไม่สามารถเข้าสู่ระบบได้"
+  def failure
+    redirect_to root_path, alert: "การยืนยันตัวตนล้มเหลว"
   end
-end
-
-def failure
-  redirect_to root_path, alert: "การยืนยันตัวตนล้มเหลว"
 end
