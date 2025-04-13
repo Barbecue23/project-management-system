a0bedd03513ccb2d67bddd0727d9af74882c1f82
Soratun <tunzax014@gmail.com>
2025-04-13 19:36:21 +0700

    Update after_sign_in_path_for method to redirect to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || some_dashboard_path
+    stored_location_for(resource) || root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
