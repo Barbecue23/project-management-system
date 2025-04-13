a63327e8b9d8b913948ca688750285b969342083
Soratun <tunzax014@gmail.com>
2025-04-13 18:12:56 +0700

    Implement user authentication check in ApplicationController and skip for HomeController index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,16 @@ class ApplicationController < ActionController::Base
   # เพิ่มบรรทัดนี้เพื่อ skip CSRF เฉพาะ callback จาก OmniAuth
   skip_before_action :verify_authenticity_token, if: -> { request.path.start_with?("/users/auth/") }
 
+  before_action :authenticate_user!
+
   def after_sign_out_path_for(resource_or_scope)
     root_path # หรือเปลี่ยนเป็น path ที่คุณต้องการ
+  end
+
+  def authenticate_user!
+    unless user_signed_in?
+      flash[:alert] = "กรุณาเข้าสู่ระบบก่อนใช้งาน"
+      redirect_to root_path
+    end
   end
 end
