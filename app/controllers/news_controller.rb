4aaa00907a32f1254dc139d58dbc4fdcf28c6fa7
Soratun <tunzax014@gmail.com>
2025-04-15 08:53:27 +0700

    Refactor news index to use Active Storage for banner images and improve content rendering

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,52 +1,6 @@
 class NewsController < ApplicationController
   def index
-    @news = [
-      {
-        title: "การขึ้นทะเบียนนักศึกษา",
-        content: "SU-TCAS68 รอบ 1 แฟ้มสะสมผลงาน (Portfolio)",
-        publish_date: "2 ชั่วโมงที่แล้ว",
-        announcer: "ศิลปากร",
-        banner_image: "news1.svg",
-        category: "Education",
-        more_images: []
-      },
-      {
-        title: "งานทะเบียนและประมวลผล งดให้บริการ",
-        content: "ปิดให้บริการชั่วคราวในวันที่ 13 - 14 กุมภาพันธ์ 2568",
-        publish_date: "1 ชั่วโมงที่แล้ว",
-        announcer: "ศิลปากร",
-        banner_image: "news2.jpg",
-        category: "Education",
-        more_images: []
-      },
-      {
-        title: "กำหนดการวันสุดท้ายของการถอนติด W",
-        content: "ต้องดำเนินการก่อน 17 ตุลาคม 2568 เวลา 16.30 น.",
-        publish_date: "1 ชั่วโมงที่แล้ว",
-        announcer: "ศิลปากร",
-        banner_image: "news3.png",
-        category: "Education",
-        more_images: []
-      },
-      {
-        title: "กิจกรรมบริจาคโลหิต 2568",
-        content: "ร่วมบริจาคโลหิตได้ที่ คณะ 016 มหาวิทยาลัยศิลปากร",
-        publish_date: "1 ชั่วโมงที่แล้ว",
-        announcer: "ศิลปากร",
-        banner_image: "news4.svg",
-        category: "Education",
-        more_images: []
-      },
-      {
-        title: "SU: Next Step รุ่น 1 ประจำปีการศึกษา 2568",
-        content: "โครงการเรียนล่วงหน้าสำหรับนักศึกษาหลักสูตร (4+1)",
-        publish_date: "1 ชั่วโมงที่แล้ว",
-        announcer: "ศิลปากร",
-        banner_image: "news5.jpg",
-        category: "Education",
-        more_images: []
-      }
-    ]
+    @news = News.includes(:banner_image_attachment).order(publish_date: :desc)
   end
 
   def new
